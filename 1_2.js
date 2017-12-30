774b6f9a88d4e72da7858a4b5d8bc71af74da40b
aVillarejo <32025718+aVillarejo@users.noreply.github.com>
2017-12-29 19:02:30 -0600

    Add files via upload

---
+++ 1_2.js
@@ -0,0 +1,29 @@
+// console.log("El area de un triangulo de -base 5 y altura 7 es:"+5*7/2)
+
+
+let base = 5
+let altura = 7
+// console.log(`El area de un triangulo de base 5 y altura 7 es: ${base*altura/2}`)
+
+const Area_Triangulo = (a,b) => {
+	let total = (a*b)/2
+	return total
+}
+
+const Area_Circulo = (a) => {
+	let pi=3.1415926536
+	let total = (pi*(a*a))
+	return total
+}
+
+console.log(`El area de un triangulo
+debase 5 y altura 7 es: ${Area_Triangulo(17,84)}`)
+
+//funcion de JavaScript
+// function AreaTriangulo(a,b) {
+// 	let total = base*altura/2
+// 	return total
+// }
+
+
+
