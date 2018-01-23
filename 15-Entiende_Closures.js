23c19226c36ce903e1837d132739ef9b5c907854
“a_villarejo19” <a.villarejo19@gmail.com>
2018-01-22 18:11:57 -0600

    3

--- 15-Entiende_Closures.js
+++ 15-Entiende_Closures.js
@@ -1,8 +1,6 @@
-
 // function saludarGomez(nombre) {
 // 	console.log(`Hola ${nombre} Gomez`)
 // }
-
 
 // function saludarPerez(nombre) {
 // 	console.log(`Hola ${nombre} Perez`)
@@ -14,24 +12,35 @@ function saludarFamilia(apellido) {
 	}
 }
 
-function saludarFamilia(apellido) {
-	return function saludarMiembro(nombre) {
+ const saludarFamilia = (apellido) => {
+	return  saludarMiembro = (nombre)=> {
 		console.log(`Hola ${nombre} ${apellido}`)
 	}
 }
 
+
+function makePrefixer(prefijo){
+	return function palabra (str){
+		console.log(`${prefijo}${str}`)
+	}
+}
+
+const makePrefixerArrow = (prefijo) => palabra = (str) => console.log(`${prefijo}${str}`)
+
+
+const PrefijoRe = makePrefixer('re')
+PrefijoRe('malo')
+
+
 const saludarGomez = saludarFamilia('Gomez')
 const saludarPerez = saludarFamilia('Perez')
-
-const saludarRomero =saludarFamilia('Romero');
+const saludarRomero = saludarFamilia('Romero');
 
 saludarGomez("Pedro");
-saludarGomez('Juan');
-saludarGomez("Beto");
 
 saludarPerez("Pedro");
-saludarPerez('Juan');
-saludarPerez("Beto");
 
 saludarRomero("Alfredo");
+
+///////////
 
